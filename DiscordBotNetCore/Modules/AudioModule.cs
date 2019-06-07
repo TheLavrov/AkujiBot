a53ba49d367e340eb3cdd5b2f53f9a2002131085
Alexander Lavrov <alavrov@csu.fullerton.edu>
2019-06-07 14:42:15 -0700

    Added ~setavatar

--- DiscordBotNetCore/Modules/AudioModule.cs
+++ DiscordBotNetCore/Modules/AudioModule.cs
@@ -50,61 +50,61 @@ public class AudioModule : ModuleBase<ICommandContext>
     //    }
 
     [Command("bup", RunMode = RunMode.Async)]
-[Summary("bup")]
-public async Task Bup()
-{
-    if (DateTimeOffset.UtcNow - buptimeout >= TimeSpan.FromMinutes(0.25))
+    [Summary("bup")]
+    public async Task Bup()
     {
-        buptimeout = DateTimeOffset.UtcNow;
-        await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
-        await _service.SendAudioAsync(Context.Guild, Context.Channel, "bup.mp3");
-        await _service.LeaveAudio(Context.Guild);
-    }
-    else
-    {
-        var message = await ReplyAsync($"`You're bupping too hard.`");
-        await Task.Delay(1500);
-        await message.DeleteAsync();
-    }
-}
-
-[Command("vol")]
-[Remarks("vol [number]")]
-[Summary("Changes the volume for audio related commands.")]
-public async Task SetVolume(string vol)
-{
-    if (vol.All(Char.IsDigit))
-    {
-        double v = Convert.ToDouble(vol);
-        if (v < 1 && v > 0)
+        if (DateTimeOffset.UtcNow - buptimeout >= TimeSpan.FromMinutes(0.25))
         {
-            Config c = Config.Load();
-            c.Volume = Convert.ToInt32(v * 100);
-            c.SaveJson();
-        }
-        else if (v < 0)
-        {
-            Config c = Config.Load();
-            c.Volume = 0;
-            c.SaveJson();
-        }
-        else if (v > 100)
-        {
-            Config c = Config.Load();
-            c.Volume = 100;
-            c.SaveJson();
+            buptimeout = DateTimeOffset.UtcNow;
+            await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+            await _service.SendAudioAsync(Context.Guild, Context.Channel, "bup.mp3");
+            await _service.LeaveAudio(Context.Guild);
         }
         else
         {
-            Config c = Config.Load();
-            c.Volume = Convert.ToInt32(v);
-            c.SaveJson();
+            var message = await ReplyAsync($"`You're bupping too hard.`");
+            await Task.Delay(1500);
+            await message.DeleteAsync();
         }
-        await ReplyAsync($"`Volume has been set to {Config.Load().Volume}%.`");
     }
-    else
+
+    [Command("vol")]
+    [Remarks("vol [number]")]
+    [Summary("Changes the volume for audio related commands.")]
+    public async Task SetVolume(string vol)
     {
-        await ReplyAsync($"`Invalid volume.`");
+        if (vol.All(Char.IsDigit))
+        {
+            double v = Convert.ToDouble(vol);
+            if (v < 1 && v > 0)
+            {
+                Config c = Config.Load();
+                c.Volume = Convert.ToInt32(v * 100);
+                c.SaveJson();
+            }
+            else if (v < 0)
+            {
+                Config c = Config.Load();
+                c.Volume = 0;
+                c.SaveJson();
+            }
+            else if (v > 100)
+            {
+                Config c = Config.Load();
+                c.Volume = 100;
+                c.SaveJson();
+            }
+            else
+            {
+                Config c = Config.Load();
+                c.Volume = Convert.ToInt32(v);
+                c.SaveJson();
+            }
+            await ReplyAsync($"`Volume has been set to {Config.Load().Volume}%.`");
+        }
+        else
+        {
+            await ReplyAsync($"`Invalid volume.`");
+        }
     }
-}
 }
--- DiscordBotNetCore/Modules/InfoModules.cs
+++ DiscordBotNetCore/Modules/InfoModules.cs
@@ -4,7 +4,10 @@ using Discord;
 using Discord.Commands;
 using System.IO;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Net;
+using System.Globalization;
 
 namespace DiscordBot.Modules
 {
@@ -17,7 +20,7 @@ namespace DiscordBot.Modules
 		{
 			var embed = new EmbedBuilder();
 			embed.WithAuthor($"About the Bot", Context.Client.CurrentUser.GetAvatarUrl(), "http://ko-fi.com/akujithesniper");
-			embed.WithColor(Color.Gold);
+			embed.WithColor(Discord.Color.Gold);
 			string info = $"Hello, I am a Discord bot written in **Discord.Net version {DiscordConfig.Version}**." +
 						$"\nThis bot is currently running on: **{RuntimeInformation.OSDescription} ({RuntimeInformation.OSArchitecture})**.";
 			embed.WithDescription(info);
@@ -51,18 +54,56 @@ namespace DiscordBot.Modules
 			await ReplyAsync($"`Here's a list of our current admins:`\n\n{list}");
 		}
 
-		//just a test to see how changing a nickname works
-		/*
-		[Command("nickname")]
-		[Summary("test")]
-		public async Task Nickname()
-		{
-			await Context.Client.CurrentUser.ModifyAsync(x =>
-			{
-				x.Avatar = new Image(File.OpenRead("avatar.jpg"));
-				x.Username = "";
-			});
-		}
-		*/
-	}
+        //just a test to see how changing a nickname works
+
+        [Command("setavatar")]
+        [Remarks("setavatar [image url]")]
+        [Summary("(ADMIN ONLY) Set the bot's avatar.")]
+        public async Task Nickname([Remainder] string url)
+        {
+            if (!Config.Load().IsAdmin(Context.User.Id))            //if they arent an admin, turn them down
+            {
+                var message = await ReplyAsync($"`Only admins can use this command.`");
+                await Task.Delay(2000);
+                await message.DeleteAsync();
+                return;
+            }
+
+            bool IsImageUrl(string URL)
+            {
+                var req = (HttpWebRequest)HttpWebRequest.Create(URL);
+                req.Method = "HEAD";
+                using (var resp = req.GetResponse())
+                {
+                    return resp.ContentType.ToLower(CultureInfo.InvariantCulture)
+                               .StartsWith("image/");
+                }
+            }
+
+            if (IsImageUrl(url))
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    using (Stream stream = webClient.OpenRead(url))
+                    {
+                        await Context.Client.CurrentUser.ModifyAsync(x =>
+                        {
+                            x.Avatar = new Image(stream);
+                        });
+                        var message = await ReplyAsync($"`Avatar set.`");
+                        await Task.Delay(2000);
+                        await message.DeleteAsync();
+                    }
+                }
+            }
+            else
+            {
+                var message = await ReplyAsync($"`Unable to load image.`");
+                await Task.Delay(2000);
+                await message.DeleteAsync();
+            }
+            
+        }
+
+    }
 }
